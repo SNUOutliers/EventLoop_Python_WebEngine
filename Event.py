6b0ea790e3be58ae47f8ba02a5335f8664725199
yoon <foryou8904@naver.com>
2017-12-16 16:02:44 +0900

    Modify constructor & Fix typo

--- Event.py
+++ Event.py
@@ -1,18 +1,13 @@
 class Event:
 	
-	def __init__(self, method, request_uri, CLIENT_SOCKET=None):
+	def __init__(self, method, request_uri, disk_io=False):
 		self.method = method
 		self.request_uri = request_uri
-		self.disk_io = False
-		self.CLIENT_SOCKET = CLIENT_SOCKET
-		self.request_bytes = None
+		self.disk_io = disk_io
+		self.CLIENT_SOCKET = None
+		self.response_bytes = None
 		self.content_type = None
 	
 	def is_disk_io(self):
 		return self.disk_io
 
-	
-
-
-		
-		
