0d2faf2de3a7d5894d935641d8d2302b19b1bc75
clubsandwich <foryou8904@snu.ac.kr>
2017-12-19 01:53:06 +0900

    Fix node file never die.

--- compare/node_app.js
+++ compare/node_app.js
@@ -1,14 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 http.createServer(function (req, res) {
-		fs.readFile('../resources' + req.url, 'utf8', function (err,data) {
-  		if (err) {
-    		return console.log(err);
-  		}
-  	 	res.writeHead(200, {'Content-Type': '*/*'});
-    	res.write(data);
-    	res.end();
-		});
+	var filePath = path.join('../resources', req.url);
+	var stat = fs.statSync(filePath);
+
+	res.writeHead(200, {
+		'Content-Type': '*/*',
+		'Content-Length': stat.size
+	});
+
+	var readStream = fs.createReadStream(filePath);
+	// We replaced all the event handlers with a simple call to readStream.pipe()
+	readStream.pipe(res);
 }).listen(8080);
 
