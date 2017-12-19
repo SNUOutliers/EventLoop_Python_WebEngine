3e2648b786240191f6fc2214005c269c36ac951a
clubsandwich <foryou8904@snu.ac.kr>
2017-12-19 21:41:04 +0900

    Set all content type as '*/*' except *.html files

--- event.py
+++ event.py
@@ -11,18 +11,11 @@ class Event:
 
 	def set_content_type(self):
 		# set content_type by uri
-		if self.request_uri.endswith('.jpeg'):
-			self.content_type = 'image/jpeg'
-		elif self.request_uri.endswith('.html'):
+		if self.request_uri.endswith('.html'):
 			self.content_type = 'text/html'
-		elif self.request_uri.endswith('.mp4'):
-			self.content_type = 'video/mp4'
-		elif self.request_uri.endswith('.zip'):
-			self.content_type = 'application/zip'
-		elif self.request_uri.endswith('.ico'):
-			self.content_type = 'image/x-icon'
 		else:
 			self.content_type = '*/*'
 
 	def is_disk_io(self):
 		return self.disk_io
+
